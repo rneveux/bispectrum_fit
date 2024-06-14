fdf2a74e2c5c52651a9f3afbf1a1e83d246e91b7
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-06-14 14:18:22 +0100

    Add  and remove temporary files from tracking

--- .ipynb_checkpoints/run_fit-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=03:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=10G
-#SBATCH --constraint=avx
-
-srun python3 bispectrum/fit/fit_test.py -config $1
